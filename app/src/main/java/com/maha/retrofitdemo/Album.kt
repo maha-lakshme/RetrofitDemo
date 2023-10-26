5e5bf826faad666d675a0845f8dfc6ec9c035019
maha-lakshme <maha21.kanagaraj@gmail.com>
2023-10-26 08:23:38 +0530

    Initial commit

---
+++ app/src/main/java/com/maha/retrofitdemo/Album.kt
@@ -0,0 +1,3 @@
+package com.maha.retrofitdemo
+
+class Album : ArrayList<AlbumItem>()
---
+++ app/src/main/java/com/maha/retrofitdemo/AlbumService.kt
@@ -0,0 +1,11 @@
+package com.maha.retrofitdemo
+
+import retrofit2.Response
+import retrofit2.http.GET
+
+interface AlbumService {
+
+    @GET("/albums")
+    suspend fun getAlbums():Response<Album>
+
+}
